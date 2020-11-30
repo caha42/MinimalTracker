23c07700a79ccd77ca68961b9d3a0905f948af16
Carina H <carina.haupt@dlr.de>
2020-11-30 23:35:52 +0100

    Added material calender view which allows to hightlight and unhighlight dates

--- app/src/main/java/caha42/mmt/MainActivity.java
+++ app/src/main/java/caha42/mmt/MainActivity.java
@@ -4,11 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
+    List<EventDay> events = new ArrayList<>();
+    List<Calendar> calendars = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        calendarView.setOnDayClickListener(new OnDayClickListener() {
+            @Override
+            public void onDayClick(EventDay eventDay) {
+                if (calendars.contains(eventDay.getCalendar())) {
+                    events.remove(eventDay);
+                    calendars.remove(eventDay.getCalendar());
+                }
+                else {
+                    events.add(eventDay);
+                    calendars.add(eventDay.getCalendar());
+                }
+                calendarView.setHighlightedDays(calendars);
+            }
+        });
     }
 }
