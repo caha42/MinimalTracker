ba7b950327f2d3b12093f6012bcd69ad9e0e3d1e
Carina H <carina.haupt@dlr.de>
2020-12-02 21:03:41 +0100

    Cleaned up code a bit

--- app/src/main/java/caha42/mmt/MainActivity.java
+++ app/src/main/java/caha42/mmt/MainActivity.java
@@ -24,17 +24,8 @@ public class MainActivity extends AppCompatActivity {
     // Projection array. Creating indices for this array instead of doing
     // dynamic lookups improves performance.
     public static final String[] EVENT_PROJECTION = new String[] {
-            CalendarContract.Calendars._ID,                           // 0
-//            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
-//            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
-//            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
+            CalendarContract.Calendars._ID
     };
-
-    // The indices for the projection array above.
-    private static final int PROJECTION_ID_INDEX = 0;
-//    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
-//    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
-//    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
 
     private static final String CALENDER_NAME = "Migraine";
     private static final String CALENDER_OWNER = "MinimalMigraineTracker";
@@ -83,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         long calID;
 
         if (cur.isFirst() && cur.isLast()) {
-            calID = cur.getLong(PROJECTION_ID_INDEX);
+            calID = cur.getLong(0);
         } else if (!cur.isFirst()) {
             calID = createCalender();
         } else {
